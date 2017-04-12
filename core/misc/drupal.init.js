e11ee482029e1f57383837bfeb062299cec4fe26
Matthew Grill <matt.grill@acquia.com>
2017-04-12 14:15:02 -0700

    drupal.init.js

--- core/misc/drupal.init.js
+++ core/misc/drupal.init.js
@@ -1,19 +1,2 @@
-// Allow other JavaScript libraries to use $.
-if (window.jQuery) {
-  jQuery.noConflict();
-}
-
-// Class indicating that JS is enabled; used for styling purpose.
-document.documentElement.className += ' js';
-
-// JavaScript should be made compatible with libraries other than jQuery by
-// wrapping it in an anonymous closure.
-
-(function (domready, Drupal, drupalSettings) {
-
-  'use strict';
-
-  // Attach all behaviors.
-  domready(function () { Drupal.attachBehaviors(document, drupalSettings); });
-
-})(domready, Drupal, window.drupalSettings);
+(function(){'use strict';window.jQuery&&jQuery.noConflict(),document.documentElement.className+=' js',function(a,b,c){a(function(){b.attachBehaviors(document,c)})}(domready,Drupal,window.drupalSettings)})();
+//# sourceMappingURL=drupal.init.js.map
