3e784eec19e74f0f7ad7e7288524c6be7ea1f326
Ted Bowman <ted.git@tedbow.com>
2017-04-17 21:07:41 -0400

    wip test

--- core/modules/quickedit/tests/src/FunctionalJavascript/QuickEditTest.php
+++ core/modules/quickedit/tests/src/FunctionalJavascript/QuickEditTest.php
@@ -7,8 +7,36 @@ use Drupal\FunctionalJavascriptTests\JavascriptTestBase;
 /**
  * Tests the UI for missing template values.
  *
- * @group contextual
+ * @group quickedit
  */
 class QuickeditTest extends JavascriptTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = ['contextual', 'field', 'filter', 'quickedit', 'node'];
+
+  /**
+   * Node for testing.
+   *
+   * @var \Drupal\node\NodeInterface
+   */
+  protected $node;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+    $this->node = $this->drupalCreateNode();
+
+  }
+
+
+  public function testMissingAttributeError() {
+    $this->drupalGet('node/' . $this->node->id());
+    $web_assert = $this->assertSession();
+    $this->assertJsCondition()
+  }
 
 }
